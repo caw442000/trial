7d14338d80dc97c593f26033137c1f29112d915f
Cedric Winbush <cwvisionnfocus@gmail.com>
2013-11-14 08:32:07 -0500

    trial again

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trial::Application.config.secret_key_base = 'b678c61bfd98feb026666f1f896e3a71c0ee87b6477b1073f88832cdeaf516a6a6f9a6caf98c179d36ca87730fe14ed6671b9e01abc9918d33f53e11efde2a81'
